eaa1ad53743ddbe601b5bc50ad3c69f6cb78d2f8
Stepanov Nikita <stepanovnv95@gmail.com>
2019-10-27 12:24:47 +0400

    Добавлены время публикации постов и превью изображение

--- main/models.py
+++ main/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
+
+def get_default_datetime():
+    return timezone.now()
+
+
+def get_upload_path(instance, filename):
+    return f'blogpost_{instance.id}/preview_image.{filename.split(".")[-1]}'
 
 
 class BlogPost(models.Model):
     title = models.TextField()
     text = models.TextField()
+    publish_date = models.DateTimeField(default=get_default_datetime)
+    preview_image = models.ImageField(upload_to=get_upload_path, null=True)
 
     def __str__(self):
         return self.title
