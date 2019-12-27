c6dcaadf1681b80d95248bbba4f9378a75b641e6
Stepanov Nikita <stepanovnv95@gmail.com>
2019-12-27 18:40:17 +0400

    Add sourcemaps to styles

--- website/tasks/styles.js
+++ website/tasks/styles.js
@@ -7,13 +7,16 @@ const rev = require('gulp-rev');
 const rev_replace = require('gulp-rev-replace');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const sourcemaps = require('gulp-sourcemaps');
 
 function make_styles(paths, manifest) {
   return function styles() {
     return gulp.src(paths.src, { base: paths.base })
       .pipe( plumber({ errorHandler: notify.onError() }) )
       .pipe( filter(['**', '!**/_*.*']) )
+      .pipe( sourcemaps.init() )
       .pipe( sass() )
+      .pipe( sourcemaps.write() )
       .pipe( rev_replace({ manifest: gulp.src(manifest.path, { allowEmpty: true }) }) )
       .pipe( rev() )
       .pipe( gulp.dest(paths.dest) )
