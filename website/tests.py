c87e33271540765818e2f335147635b9dfc10487
Stepanov Nikita <stepanovnv95@gmail.com>
2019-12-27 18:39:45 +0400

    Remove unused test

--- website/tests.py
+++ website/tests.py
@@ -1,16 +1,2 @@
 from django.test import TestCase
 
-from .models import BlogPost
-
-
-class BlogPostModelTests(TestCase):
-
-    def test_generate_media_directory_name_function(self):
-        """
-        Check generation unique media directories.
-        """
-        title = 'Test title'
-        text = 'Test text'
-        bp1 = BlogPost.objects.create(title=title, text=text)
-        bp2 = BlogPost.objects.create(title=title, text=text)
-        self.assertNotEqual(bp1.media_directory, bp2.media_directory)
