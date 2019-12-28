a196187ae212d4dc817770fed9d9127ca760e09e
Stepanov Nikita <stepanovnv95@gmail.com>
2019-12-28 17:52:18 +0400

    Remove ejs support, now will using only django templates

--- website/tasks/templates.js
+++ website/tasks/templates.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const gulp = require('gulp');
-const filter = require('gulp-filter');
-const ejs = require("gulp-ejs");
 const rev_replace = require('gulp-rev-replace');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
@@ -11,8 +9,6 @@ function make_templates(paths, manifest) {
   return function templates() {
     return gulp.src(paths.src)
       .pipe( plumber({ errorHandler: notify.onError() }) )
-      .pipe( filter(['**', '!**/_*.*']) )
-      .pipe( ejs() )
       .pipe( rev_replace({ manifest: gulp.src(manifest.path, { allowEmpty: true }) }) )
       .pipe( gulp.dest(paths.dest) );
   }
